1b97802d8fdf1925eeb276554e510654d40f41b2
shlok agrawal <shlok.agrawal@hotmail.com>
2024-06-12 14:50:45 +0530

    vercel

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use("/api/contacts", require ("./routes/contactRoutes"));
 
 app.use("/api/users", require ("./routes/userRoutes"));
 
-
+app.get('/', (req, res) => {
+    res.send('Hello from Vercel!');
+});
 app.use(errorHandler);
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
