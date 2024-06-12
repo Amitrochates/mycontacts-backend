6c6f0cc1d13f65dafa41dd712f5e963487b9dd4c
shlok agrawal <shlok.agrawal@hotmail.com>
2024-06-12 13:35:57 +0530

    deploy

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
-
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 
 
 connectDb();
 const app= express();   
+app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json()); //middleware, body parser for when client sends any data to server and 
                         //server has to parse that text, for eg, in createContacts
@@ -18,4 +19,4 @@ app.use("/api/users", require ("./routes/userRoutes"));
 app.use(errorHandler);
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
-}); 
+});     
